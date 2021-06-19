7431a3389357c9d8ae3de894679f6a0504b6dbef
ppotatoG <thgus2270@gmail.com>
2021-06-19 17:56:41 +0900

    offset 추기

--- js/sa.js
+++ js/sa.js
@@ -4,9 +4,38 @@ window.addEventListener('scroll', () => {
     for ( const saNode of saNodes){
         const rect = saNode.getBoundingClientRect();
         if(rect.top <= point) {
+            saStyleSet(saNode);  
             saNode.classList.add('show');
         } else if(rect.top > point + point*(0.1)) {
-            saNode.classList.remove('show');
+            // saNode.classList.remove('show');
         } 
+            
     }
 });
+
+function saStyleSet(saNode){
+    const match = saNode.dataset.sa.match(/from-(.*)/);
+
+    // saNode.dataset.sa 'from-'으로 시작할 때
+    if(match) { 
+        const from = match[1];
+        const offset = saNode.dataset.saOffset;
+
+        if(offset) {
+            switch(from) {
+                case 'top' :
+                    saNode.style.transform = `translate(0, -${offset})`;
+                    break;
+                case 'bottom' :
+                    saNode.style.transform = `translate(0, ${offset})`;
+                    break;
+                case 'left': 
+                    saNode.style.transform = `translate(-${offset}, 0)`;
+                    break;
+                case 'right': 
+                    saNode.style.transform = `translate(-${offset}, 0)`;
+                    break;
+            }
+        }
+    }    
+}
