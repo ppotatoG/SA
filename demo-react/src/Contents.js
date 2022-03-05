2c98d4c6f2869b844dd49112445f1c2468cccaa7
ppotatoG <thgus2270@gmail.com>
2022-03-06 01:57:59 +0900

    dataset추가 실패

--- demo-react/src/Contents.js
+++ demo-react/src/Contents.js
@@ -33,9 +33,32 @@ function Contents() {
 			'data-sa-duration': '1500',
 			'data-sa-delay': '500'
 		},
-	]
+	];
 
-	console.log(boxs)
+	const box = boxs.map((box, boxIdx) => {
+		return (		
+			<div 
+				className="box" key={boxIdx}
+				// data={Object.entries(box).map(val => val[0])}
+				// data-sa='fade'
+				// {
+				// 	...Object.entries(box).map((val, idx) => {
+				// 		console.log(val[0], val[1], idx)
+				// 		return val[0]
+				// 	})
+				// }
+			>
+				<pre>
+					{
+						Object.entries(box).map((val) => {
+							return `${val[0]}=${val[1]}\n`
+						})
+					}
+				</pre>
+			</div>
+		)
+	});
+
 	return (
 		<div className="contents">
 			<section id="main">
@@ -47,7 +70,7 @@ function Contents() {
             </section>
 
 			<section>
-
+				{box}
 			</section>
 		</div>
 	);
