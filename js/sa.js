200a59c038752d5355320481ba69701d5177ef14
ppotatoG <thgus2270@gmail.com>
2022-03-23 17:59:12 +0900

    saStyleSet 삭제

--- js/sa.js
+++ js/sa.js
@@ -4,7 +4,6 @@ window.addEventListener('scroll', () => {
     for ( const saNode of saNodes){
         const rect = saNode.getBoundingClientRect();
         if(rect.top <= point) {
-            saStyleSet(saNode);  
             saNode.classList.add('show');
         } else if(saNode.dataset.saOnce === 'false'){
             if(rect.top > point + point*(0.1)) {
@@ -14,30 +13,3 @@ window.addEventListener('scroll', () => {
         }
     }
 });
-
-const saStyleSet = (saNode) => {
-    const match = saNode.dataset.sa.match(/from-(.*)/);
-
-    if(match) { 
-        const from = match[1];
-        let offset = saNode.dataset.saOffset;
-
-        if(offset){
-            Number(offset) ? offset += 'px' : offset;
-            switch(from) {
-                case 'top' :
-                    saNode.style.transform = `translate(0, -${offset})`;
-                    break;
-                case 'bottom' :
-                    saNode.style.transform = `translate(0, ${offset})`;
-                    break;
-                case 'left': 
-                    saNode.style.transform = `translate(-${offset}, 0)`;
-                    break;
-                case 'right': 
-                    saNode.style.transform = `translate(-${offset}, 0)`;
-                    break;
-            }
-        }
-    }    
-}
