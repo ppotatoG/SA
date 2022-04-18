577886725d85b33db4c2f55e476aeb4dbfb3899a
ppotatoG <thgus2270@gmail.com>
2022-04-18 17:36:46 +0900

    boxs 수정

--- demo-react/src/Contents.js
+++ demo-react/src/Contents.js
@@ -1,38 +1,48 @@
 function Contents() {
 	const boxs = [
 		{'data-sa': 'fade'},
-		{'data-sa': 'from-right'},
-		{'data-sa': 'from-top'},
-		{'data-sa': 'from-bottom'},
-		{'data-sa': 'from-left'},
-		{'data-sa': 'flip-right'},
-		{'data-sa': 'flip-top'},
-		{'data-sa': 'flip-bottom'},
-		{'data-sa': 'flip-left'},
-		{'data-sa': 'scale'},
-		{
-			'data-sa': 'from-top',
-			'data-sa-duration': '2500'
-		},
-		{
-			'data-sa': 'from-bottom',
-			'data-sa-delay': '1000'
-		},
-		{
-			'data-sa': 'flip-left',
-			'data-sa-once': 'false',
-			'data-sa-offset': '50vw'
-		},
-		{
-			'data-sa': 'scale',
-			'data-sa-offset': '500',
-			'data-sa-duration': '5000'
-		},
-		{
-			'data-sa': 'fade',
-			'data-sa-duration': '1500',
-			'data-sa-delay': '500'
-		},
+        {'data-sa': 'from-right'},
+        {'data-sa': 'from-top'},
+        {'data-sa': 'from-bottom'},
+        {'data-sa': 'from-left'},
+        {'data-sa': 'flip-right'},
+        {'data-sa': 'flip-top'},
+        {'data-sa': 'flip-bottom'},
+        {'data-sa': 'flip-left'},
+        {'data-sa': 'scale'},
+        {
+            'data-sa': 'from-top',
+            'data-sa-duration': '2500'
+        },
+        {
+            'data-sa': 'from-bottom',
+            'data-sa-delay': '1000'
+        },
+        {
+            'data-sa': 'from-left',
+            'data-sa-once': 'false',
+            'data-sa-offset': '50'
+        },
+        {
+            'data-sa': 'from-right',
+            'data-sa-once': 'false',
+            'data-sa-offset': '500'
+        },
+        {
+            'data-sa': 'scale',
+            'data-sa-duration': '5000',
+            'data-sa-time': 'ease-out'
+        },
+        {
+            'data-sa': 'scale',
+            'data-sa-duration': '5000',
+            'data-sa-time': ' ease-in'
+        },
+        {
+            'data-sa': 'fade',
+            'data-sa-duration': '1500',
+            'data-sa-delay': '500'
+        },
 	];
 
 	const box = boxs.map((box, boxIdx) => {
