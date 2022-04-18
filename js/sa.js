7e7a9228c88e917fe4c27203bca60003d012c4bd
ppotatoG <thgus2270@gmail.com>
2022-04-18 22:00:25 +0900

    delay setTimeout로 변경

--- js/sa.js
+++ js/sa.js
@@ -4,12 +4,18 @@ const saInit = () => {
     for ( const saNode of saNodes){
         const rect = saNode.getBoundingClientRect();
         if(rect.top <= point) {
-            saNode.classList.add('saShow');
+            if(saNode.dataset.saDelay) {
+                setTimeout(() => {
+                    saNode.classList.add('saShow');
+                    }, saNode.dataset.saDelay)
+            }
+            else {
+                saNode.classList.add('saShow');
+            }
         } else if(saNode.dataset.saOnce === 'false'){
             if(rect.top > point + point*(0.1)) {
                 saNode.classList.remove('saShow');
             }
-            
         }
     }
 }
