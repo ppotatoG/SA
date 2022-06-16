c9d2f7333de55faef5eb22b867ac30197ca1d9fa
ppotatoG <thgus2270@gmail.com>
2022-06-16 23:40:55 +0900

    Add maintext

--- demo-react/src/Contents.js
+++ demo-react/src/Contents.js
@@ -1,6 +1,8 @@
-function Contents() {
-	const boxs = [
-		{'data-sa': 'fade'},
+import React, { useEffect } from 'react';
+
+const Contents = () => {
+    const boxs = [
+        {'data-sa': 'fade'},
         {'data-sa': 'from-right'},
         {'data-sa': 'from-top'},
         {'data-sa': 'from-bottom'},
@@ -43,40 +45,72 @@ function Contents() {
             'data-sa-duration': '1500',
             'data-sa-delay': '500'
         },
-	];
-
-	const box = boxs.map((box, boxIdx) => {
-		return (		
-			<div  
-				className="box" 
-				key={boxIdx}
-				{...box}
-			>
+    ];
+    const box = boxs.map((box, boxIdx) => {
+        return (
+            <div
+                className="box"
+                key={boxIdx}
+                {...box}
+            >
 				<pre>
 					{
-						Object.entries(box).map((val) => {
-							return `${val[0]}="${val[1]}"\n`
-						})
-					}
+                        Object.entries(box).map((val) => {
+                            return `${val[0]}="${val[1]}"\n`
+                        })
+                    }
 				</pre>
-			</div>
-		)
-	});
+            </div>
+        )
+    });
 
-	return (
-		<div className="contents">
+    useEffect(() => {
+        const height = mainText.offsetHeight;
+        let flag = false;
+        let i = 1;
+
+        const loop = setInterval(() => {
+            // mainText.style.transform = `translateY(-${height * i}px)`;
+            console.log(flag)
+            if ( flag ) {
+                if ( i === 0 ) {
+                    flag = false;
+                    i++;
+                    return;
+                }
+                i--;
+            } else {
+                if ( i === mainTextArr.length - 1 ) {
+                    flag = true;
+                    i--;
+                    return;
+                }
+                i++;
+            }
+        }, 4000);
+    })
+
+    const mainTextArr = ['easy', 'simple'];
+    const mainText = mainTextArr.map((text, idx) => {
+        return <li key={idx}>{text}</li>
+    })
+
+    return (
+        <div className="contents">
             <section id="main">
                 <h2>
-                    <div className="mainText"></div>
+                    <div className="mainText">
+                        <ul>{mainText}</ul>
+                    </div>
                     <p>Scroll Animation</p>
                 </h2>
             </section>
 
             <section>
-				{box}
-			</section>
-		</div>
-	);
+                {box}
+            </section>
+        </div>
+    );
 }
 
 export default Contents;
