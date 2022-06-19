2c4af63b34b4227f86aa4d5e47933982416e6aa4
ppotatoG <thgus2270@gmail.com>
2022-06-19 18:32:49 +0900

    Change Show point

--- src/js/sa.js
+++ src/js/sa.js
@@ -1,19 +1,20 @@
 const saInit = () => {
-    const saNodes = document.querySelectorAll("[data-sa]");
+    const saNodes = document.querySelectorAll('[data-sa]');
     const point = window.innerHeight;
-    for ( const saNode of saNodes){
+    for (const saNode of saNodes) {
         const rect = saNode.getBoundingClientRect();
-        if(rect.top <= point) {
+        if(rect.top <= point - point * .1) {
             if(saNode.dataset.saDelay) {
                 setTimeout(() => {
                     saNode.classList.add('saShow');
-                    }, saNode.dataset.saDelay)
+                }, saNode.dataset.saDelay)
             }
             else {
                 saNode.classList.add('saShow');
             }
-        } else if(saNode.dataset.saOnce === 'false'){
-            if(rect.top > point + point*(0.1)) {
+        }
+        else if(saNode.dataset.saOnce === false) {
+            if(rect.top > point) {
                 saNode.classList.remove('saShow');
             }
         }
