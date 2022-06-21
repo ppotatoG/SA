4bd228073065beab0bd545bd3b6078e636587f56
ppotatoG <thgus2270@gmail.com>
2022-06-21 22:23:01 +0900

    Compile with rollup

--- dist/sa.js
+++ dist/sa.js
@@ -1,24 +1,25 @@
-(function (factory) {
-    typeof define === 'function' && define.amd ? define(factory) :
-    factory();
-})((function () { 'use strict';
+(function () {
+    'use strict';
 
     const saInit = () => {
-        const saNodes = document.querySelectorAll("[data-sa]");
+        const saNodes = document.querySelectorAll('[data-sa]');
         const point = window.innerHeight;
-        for ( const saNode of saNodes){
+        for (const saNode of saNodes) {
             const rect = saNode.getBoundingClientRect();
-            if(rect.top <= point) {
+
+            if(rect.top <= point - point * .1) {
                 if(saNode.dataset.saDelay) {
                     setTimeout(() => {
                         saNode.classList.add('saShow');
-                        }, saNode.dataset.saDelay);
+                    }, saNode.dataset.saDelay);
                 }
                 else {
                     saNode.classList.add('saShow');
                 }
-            } else if(saNode.dataset.saOnce === 'false'){
-                if(rect.top > point + point*(0.1)) {
+            }
+
+            if(saNode.dataset.saOnce === 'false') {
+                if((rect.top < point * -1 || rect.top > point)) {
                     saNode.classList.remove('saShow');
                 }
             }
@@ -27,4 +28,4 @@
 
     window.addEventListener('scroll', saInit);
 
-}));
+})();
