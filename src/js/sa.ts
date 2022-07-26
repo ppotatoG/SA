c071bd08f11c9c70afcd616712a057bb8d5800b4
ppotatoG <thgus2270@gmail.com>
2022-07-26 18:27:27 +0900

    Change typescript

--- src/js/sa.ts
+++ src/js/sa.ts
@@ -1,31 +1,41 @@
-"use strict";
-const sa = {
-    saNodes: [],
+interface saObj {
+    saNodes: NodeListOf<HTMLElement> | [];
+    viewPonit: Number;
+    scroll: Function;
+    init: Function;
+}
+
+const sa : saObj = {
+    saNodes: [], 
     viewPonit: .1,
-    scroll: function () {
-        if (this.saNodes && this.saNodes.length > 0) {
+    scroll: function() {
+        if(this.saNodes.length > 0) {
             const point = window.innerHeight;
-            this.saNodes.forEach((saNode) => {
+            this.saNodes.forEach((saNode : HTMLElement) => {
                 const rect = saNode.getBoundingClientRect();
                 const viewPonit = saNode.dataset.viewPonit || this.viewPonit;
-                if (rect.top <= point - +viewPonit) {
+    
+                if(rect.top <= point - (+ viewPonit)) {
                     saNode.classList.add('saShow');
                 }
-                if (saNode.dataset.saOnce === 'false') {
-                    if ((rect.top < point - +viewPonit || rect.top > point)) {
+    
+                if(saNode.dataset.saOnce === 'false') {
+                    if((rect.top < point - (+ viewPonit) || rect.top > point)) {
                         saNode.classList.remove('saShow');
                     }
                 }
-            });
+            });          
         }
     },
-    init: function (el) {
+    init: function(el : string) {
         this.saNodes = document.querySelectorAll(el);
     }
-};
+}
+
 window.addEventListener('DOMContentLoaded', () => {
     sa.init('[data-sa]');
-});
+})
+
 window.addEventListener('scroll', () => {
     sa.scroll();
 });
