68a8526268886197619075ad769d0c675f79d0e9
ppotatoG <thgus2270@gmail.com>
2022-11-16 14:37:46 +0900

    trim

--- src/js/sa.ts
+++ src/js/sa.ts
@@ -5,51 +5,47 @@ interface saObj {
     init: Function;
 }
 
-const sa : saObj = {
+const sa: saObj = {
     saNodes: [],
-    setThreshold: function(value : string | number | undefined) : number | undefined {
-        if(value) {
-            if(Number(value)) {
+    setThreshold: function (value: string | number | undefined): number | undefined {
+        if (value) {
+            if (Number(value)) {
                 return Number(value);
+            } else if ((/[0-9]+%/).test(value.toString())) {
+                return Number(100 * (+value / 100));
             }
-
-            else if((/[0-9]+%/).test(value.toString())) {
-                return Number(100 * (+ value / 100));
-            }
-        }
-
-        else {
+        } else {
             return Number(.1);
         }
     },
-    start: function(test : string | number | undefined) : void {
-        if(this.saNodes.length > 0) {
+    start: function (test: string | number | undefined): void {
+        if (this.saNodes.length > 0) {
             const point = window.innerHeight;
-            this.saNodes.forEach((saNode : HTMLElement) : void => {
+            this.saNodes.forEach((saNode: HTMLElement): void => {
                 const rect = saNode.getBoundingClientRect();
                 const threshold = this.setThreshold(test);
-    
-                if(rect.top < point - (+ threshold * point)) {
+
+                if (rect.top < point - (+threshold * point)) {
                     saNode.classList.add('saShow');
                 }
-    
-                if(saNode.dataset.saOnce === 'false') {
-                    if((rect.top < point - (+ threshold) || rect.top > point)) {
+
+                if (saNode.dataset.saOnce === 'false') {
+                    if ((rect.top < point - (+threshold) || rect.top > point)) {
                         saNode.classList.remove('saShow');
                     }
                 }
             });
         }
     },
-    init: function(el : string) : void {
+    init: function (el: string): void {
         this.saNodes = document.querySelectorAll(el);
     }
 }
 
-window.addEventListener('DOMContentLoaded', () : void => {
+window.addEventListener('DOMContentLoaded', (): void => {
     sa.init('[data-sa]');
 })
 
-window.addEventListener('scroll', () : void => {
+window.addEventListener('scroll', (): void => {
     sa.start(0);
 });
