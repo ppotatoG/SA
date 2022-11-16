9b5b4ef78ab0f2f4e69ea828ba3e8d03194db4d4
ppotatoG <thgus2270@gmail.com>
2022-11-16 18:03:10 +0900

    add: IntersectionObserver

--- src/js/sa.js
+++ src/js/sa.js
@@ -1,31 +1,64 @@
+/**
+
+ node 생성
+ node별 Threshold 세팅
+ 최고 시작
+ 초기화
+ 값 다시 찾기
+
+ Intersection_Observer_API
+ 1. node 변수에 담기
+ 2. 1별 옵저버 옵션 변수 저장
+ 3. 1, 2를 기준으로 옵저버 실행
+ - 전역 threshold, node offset-position
+ - 1. rootMargin
+ - 2. threshold
+ - 3. once
+ **/
 var sa = {
     saNodes: [],
     threshold: .1,
-    start: function () {
-        var _this = this;
-        if (this.saNodes.length > 0) {
-            var point_1 = window.innerHeight;
-            this.saNodes.forEach(function (saNode, i) {
-                var rect = saNode.getBoundingClientRect();
-                var threshold = saNode.dataset.saThreshold || _this.threshold;
-                if (rect.top < point_1 - (+threshold * point_1)) {
-                    saNode.classList.add('saShow');
-                }
-                if (saNode.dataset.saOnce === 'false') {
-                    if ((rect.top < point_1 - (+threshold) || rect.top > point_1)) {
-                        saNode.classList.remove('saShow');
-                    }
-                }
-            });
+    setThreshold: function (param) {
+        if (!isNaN(param)) {
+            return Number(param);
         }
+        else if ((/[0-9]+%/).test(param.toString())) {
+            return Number(100 * (+param / 100));
+        }
+        return null;
     },
-    init: function (el) {
+    intersection: function () {
+        console.log('intersection');
+        io.observe(this.saNodes);
+    },
+    init: function (el, threshold) {
+        console.log('init');
         this.saNodes = document.querySelectorAll(el);
+        console.log(this.saNodes);
+        if (threshold)
+            this.setThreshold(threshold);
+        this.intersection();
     }
 };
+var io = new IntersectionObserver(function (nodes, observer) {
+    nodes.forEach(function (node) {
+        var target = node.target;
+        console.log(target);
+        console.dir(target);
+        // if (node.target.dataset.saOnce === 'false') {
+        //     if (node.isIntersecting) {
+        //         target.classList.add('saShow');
+        //     }
+        //     target.classList.remove('saShow');
+        // }
+        if (node.isIntersecting) {
+            target.classList.add('saShow');
+            observer.unobserve(target);
+        }
+    });
+}, {
+    threshold: sa.threshold
+});
 window.addEventListener('DOMContentLoaded', function () {
     sa.init('[data-sa]');
-});
-window.addEventListener('scroll', function () {
-    sa.start();
 });
