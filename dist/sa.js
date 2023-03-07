26dc4bbf3c00587daa8cc8a61b424a02ccf58bf9
ppotatoG <thgus2270@gmail.com>
2023-03-07 11:46:13 +0900

    Build

--- dist/sa.js
+++ dist/sa.js
@@ -1,31 +1,72 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  const init = () => {
-    const saNodes = document.querySelectorAll('[data-sa]');
-    const point = window.innerHeight;
-    for (const saNode of saNodes) {
-      const rect = saNode.getBoundingClientRect();
+    const sa = {
+        saNodes: null,
+        threshold: 0.1,
+        setThreshold: function (threshold) {
+            if (typeof threshold === 'string') {
+                threshold = threshold.trim();
+                if (threshold === '') {
+                    console.warn('threshold value is empty, using default value 0.1');
+                    return 0.1;
+                }
+                if (isNaN(Number(threshold))) {
+                    console.warn(`invalid threshold value "${threshold}", using default value 0.1`);
+                    return 0.1;
+                }
+                threshold = Number(threshold);
+            }
+            if (typeof threshold !== 'number') {
+                console.warn(`invalid threshold value "${threshold}", using default value 0.1`);
+                return 0.1;
+            }
+            if (threshold > 1) {
+                console.warn(`threshold value "${threshold}" cannot be greater than 1, using maximum value 1`);
+                threshold = 1;
+            }
+            return threshold;
+        },
+        intersection: function () {
+            if (this.saNodes && this.saNodes.length) {
+                this.saNodes.forEach((saNode) => io.observe(saNode));
+            }
+        },
+        init: function (threshold) {
+            const nodes = document.querySelectorAll('[data-sa]');
+            if (!nodes || nodes.length === 0) {
+                throw new Error('No matching elements found');
+            }
+            this.saNodes = nodes;
+            if (threshold !== undefined) {
+                this.threshold = this.setThreshold(threshold);
+            }
+            this.intersection();
+        },
+    };
+    const io = new IntersectionObserver((nodes) => {
+        nodes.forEach((node) => {
+            const target = node.target;
+            if (!(target instanceof HTMLDivElement))
+                return false;
+            const once = target.dataset.saOnce === 'false';
+            if (once) {
+                if (node.isIntersecting) {
+                    target.classList.add('saShow');
+                }
+                else if (!node.isIntersecting && target.classList.contains('saShow')) {
+                    target.classList.remove('saShow');
+                }
+            }
+            else {
+                if (node.isIntersecting) {
+                    target.classList.add('saShow');
+                    io.unobserve(target);
+                }
+            }
+        });
+    }, {
+        threshold: sa.threshold,
+    });
 
-      if (rect.top <= point - point * 0.1) {
-        if (saNode.dataset.saDelay) {
-          setTimeout(() => {
-            saNode.classList.add('saShow');
-          }, saNode.dataset.saDelay);
-        } else {
-          saNode.classList.add('saShow');
-        }
-      }
-
-      if (saNode.dataset.saOnce === 'false') {
-        if (rect.top < point * -1 || rect.top > point) {
-          saNode.classList.remove('saShow');
-        }
-      }
-    }
-  };
-
-  window.addEventListener('scroll', init);
-
-  return init;
 })();
