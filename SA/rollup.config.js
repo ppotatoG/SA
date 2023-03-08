14a7cde98df392ad4d55bc0a095092bad96f1f72
ppotatoG <thgus2270@gmail.com>
2023-03-08 23:15:07 +0900

    Refactor: change folder path

--- SA/rollup.config.js
+++ SA/rollup.config.js
@@ -1,7 +1,12 @@
+import { terser } from "rollup-plugin-terser";
+
 export default {
   input: './src/js/sa.js',
   output: {
     file: './dist/sa.js',
     format: 'iife',
   },
+  plugins: [
+    terser(),
+  ],
 };
