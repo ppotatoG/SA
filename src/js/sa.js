aecd4d87be8c4864e19cf8580c2fb2ebb14f7fee
ppotatoG <thgus2270@gmail.com>
2023-03-12 20:25:58 +0900

    Cherry-pick: bb3cb9c5

--- src/js/sa.js
+++ src/js/sa.js
@@ -1,4 +1,5 @@
-var sa = {
+"use strict";
+const sa = {
     /** 교차점을 지나는 대상 요소들을 저장할 NodeList */
     saNodes: [],
     /** Scroll 위치와 대상 요소의 교차점을 판단하는 기준값 */
@@ -12,21 +13,21 @@ var sa = {
                 threshold = 0.1;
             }
             if (!isFinite(Number(threshold))) {
-                console.warn("invalid threshold value \"".concat(threshold, "\", using default value 0.1"));
+                console.warn(`invalid threshold value "${threshold}", using default value 0.1`);
                 threshold = 0.1;
             }
             threshold = Number(threshold);
         }
         if (typeof threshold !== 'number') {
-            console.warn("invalid threshold value \"".concat(threshold, "\", using default value 0.1"));
+            console.warn(`invalid threshold value "${threshold}", using default value 0.1`);
             threshold = 0.1;
         }
         if (threshold > 1) {
-            console.warn("threshold value \"".concat(threshold, "\" cannot be greater than 1, using maximum value 1"));
+            console.warn(`threshold value "${threshold}" cannot be greater than 1, using maximum value 1`);
             threshold = 1;
         }
         else if (threshold < 0) {
-            console.warn("threshold value \"".concat(threshold, "\" cannot be less than 0, using minimum value 0"));
+            console.warn(`threshold value "${threshold}" cannot be less than 0, using minimum value 0`);
             threshold = 0;
         }
         sa.threshold = threshold;
@@ -34,15 +35,14 @@ var sa = {
     /** intersection observer 등록 함수 */
     intersection: function () {
         if (sa.saNodes && sa.saNodes.length) {
-            for (var _i = 0, _a = sa.saNodes; _i < _a.length; _i++) {
-                var saNode = _a[_i];
+            for (const saNode of sa.saNodes) {
                 io.observe(saNode);
             }
         }
     },
     /** 플러그인 초기화 함수 */
     init: function (threshold) {
-        var nodes = document.querySelectorAll('[data-sa]');
+        const nodes = document.querySelectorAll('[data-sa]');
         if (!nodes || nodes.length === 0) {
             throw new Error('No matching elements found');
         }
@@ -54,13 +54,13 @@ var sa = {
     },
 };
 /** js Observer API 커스텀 함수 */
-var io = new IntersectionObserver(function (nodes) {
-    nodes.forEach(function (node) {
-        var target = node.target;
+const io = new IntersectionObserver((nodes) => {
+    nodes.forEach((node) => {
+        const target = node.target;
         if (!(target instanceof HTMLDivElement))
             return false;
         /** 애니메이션을 한 번만 실행할 것인지 여부 */
-        var once = target.dataset.saOnce !== 'true';
+        const once = target.dataset.saOnce !== 'true';
         if (once) {
             if (node.isIntersecting) {
                 target.classList.add('saShow'); // 애니메이션 클래스 추가
